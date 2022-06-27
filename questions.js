4416a6049234fb3dade7c1378a76b3d6f24c1666
Kdenn1 <103376676+Kdenn1@users.noreply.github.com>
2022-06-27 10:50:44 -0700

    added five quiz questions to their own array on a separate js document to be pulled from later

---
+++ questions.js
@@ -0,0 +1,61 @@
+// this is a separate js file for the questions to be stored in
+//i found this useful for organization purposes
+//and it makes it easier for me to understand to be honest 
+
+let questions = [
+    {
+        numb: 1,
+        question: "Inside which HTML element does the JS link belong?",
+        answer: "<script>",
+        options: [
+            "<html>",
+            "<css>",
+            "<script>",
+            "<header>"
+        ]
+    },
+    {
+        numb: 2,
+        question: "How do you grab an HTML element by its ID?",
+        answer: "document.getElementById",
+        options: [
+            "document.getThatId",
+            "document.querySelector",
+            "get.elementId",
+            "document.getElementById"
+        ]
+    },
+    {
+        numb: 3,
+        question: "We have been taught to insert the JS link _____",
+        answer: "botton of the body section of HTML",
+        options: [
+            "header section",
+            "right next to the css stylesheet link",
+            "at the bottom of the body section",
+            "don't insert the JS link, it's lame"
+        ]
+    },
+    {
+        numb: 4,
+        question: "How would you write a message in an alert box?",
+        answer: "alert('message goes here')",
+        options: [
+            "alert('message goes here')",
+            "alert-message-goes-here",
+            "alertBox(message)",
+            "alertMSG(message)"
+        ]
+    },
+    {
+        numb: 5,
+        question: "How do you create a function in Javascript?",
+        answer: "function = myFunction()",
+        options: [
+            "function = myFunction[]",
+            "function = function.start",
+            "function = myFunction()",
+            "function = function;"
+        ]
+    }
+];
--- script.js
+++ script.js
@@ -1,8 +1,16 @@
-//grabbing elements 
+//get the button query 
 const start_btn = document.querySelector(".start_btn button");
-const info_box = document.querySelector(".info_box");
 const exit_btn = info_box.querySelector(".buttons .quit");
 const continue_btn = document.querySelector(".buttons .restart");
+//get the quiz query
+const info_box = document.querySelector(".info_box");
+const option_list = document.querySelector(".option_list");
+const quiz_box = document.querySelector(".quiz_box");
+const result_box = document.querySelector(".result_box");
+//get the time query
+const time_line = document.querySelector("header .time_line");
+const timeText = document.querySelector(".timer ,timer_sec");
+const timeCount = document.querySelector(".timer .timer_sec");
 
 //function for when the start quiz button is clicked 
 start_btn.onclick = ()=>{
@@ -13,3 +21,28 @@ start_btn.onclick = ()=>{
 start_btn.onclick = ()=>{
     info_box.classList.add("activeInfo");
 }
+
+//if the continuequiz button is clicked 
+continue_btn.onclick = ()=>{
+    //need command to hide the info box 
+    info_box.classList.remove("activeInfo");
+    //need command to show the quiz box 
+    quiz_box.classList.add("activeQuiz");
+    //add essential functions to the quiz
+    showQuestions(0); 
+    queCounter(1);
+    startTimer(15);
+    startTimerLine(0);
+}
+
+//set the starting values for the quiz 
+let timeValue = 15; //in seconds 
+let que_count = 0; //for starting at question #0
+let que_numb = 1; //for starting the first question 
+let userScore = 0; //player gets a starting score of zero 
+let counter;
+let counterLine;
+let widthValue = 0;
+
+const restart_quiz = result_box.querySelector(".buttons .restart");
+const quit_quiz = result_box.querySelector(".buttons .quit");
